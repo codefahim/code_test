3abcf96acf26c7f4ec37f84fdedc92ed005794fe
Abdullah Al Fahim <50063518+fahimw@users.noreply.github.com>
2021-02-13 06:00:07 +0600

    all

---
+++ Map,loop/map.js
@@ -0,0 +1,39 @@
+const company = [{
+    name: 'fahim',
+    age: 26,
+    location: 'coxs bazar',
+    country: 'bangladesh'
+}, {
+    name: 'fahim',
+    age: 26,
+    location: 'coxs bazar',
+    country: 'bangladesh'
+}, {
+    name: 'fahim',
+    age: 26,
+    location: 'coxs bazar',
+    country: 'bangladesh'
+}, {
+    name: 'fahim',
+    age: 26,
+    location: 'coxs bazar',
+    country: 'bangladesh'
+}, {
+    name: 'fahim',
+    age: 26,
+    location: 'coxs bazar',
+    country: 'bangladesh'
+}];
+
+// for (let i = 0; i < company.length; i++) {
+//     const element = company[i];
+//     console.log(element);
+
+// }
+// company.forEach(element => {
+//     console.log(element);
+// })
+
+// company.map(element => {
+//     console.log(element);
+// })
